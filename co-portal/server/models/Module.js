e18033a53c541c1dad7b659ec1c2f1e3ec587914
Unknown <nomakhosazanankosi@gmail.com>
2018-08-06 14:41:44 +0200

    Added the module model

---
+++ co-portal/server/models/Module.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+var Schema = mongoose.Schema;
+
+const ModuleSchema = new mongoose.Schema({
+    _id: {
+        type: Schema.Types.ObjectId,
+        default: mongoose.Types.ObjectId()
+    },
+    name: String,
+    code: String,
+    description: String,
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    removed: {
+        type: Boolean,
+        default: false
+    },
+
+    //Relationships
+    notes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'LectureNote'
+    }],
+    questionaires: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Questionaire'
+    }],
+    students: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Student'
+    }],
+    lecturers: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Lecturer'
+    }],
+});
+
+ModuleSchema.methods.findSimilarTypes = function (cb) {
+    return this.model('Animal').find({
+        type: this.type
+    }, cb);
+};
+
+const Module = mongoose.model('Module', ModuleSchema);
+module.exports = Module;
