42308a317055015e96dfe00f1c21bbed776a9a22
Unknown <joe@gmail.com>
2018-10-08 22:06:46 +0200

    Added marksheet model

---
+++ co-portal/server/models/MarkSheet.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+var Schema = mongoose.Schema;
+
+const MarkSheet = new mongoose.Schema({
+    _id: {
+        type: Schema.Types.ObjectId,
+        default: mongoose.Types.ObjectId()
+    },
+    lecturerID: {
+        type: Schema.Types.ObjectId,
+        ref: 'Lecturer'
+    },
+    moduleID: {
+        type: Schema.Types.ObjectId,
+        ref: 'Module'
+    },
+    id: String,
+    title: String,
+    type: {
+        type: String,
+        enum: ['PRACTICALTEST', 'THEORYTEST', 'HOMECLASSTEST', 'EXAM', 'ONLINETEST', 'UNKNOWN'],
+        default: 'UNKNOWN'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    removed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+MarkSheetSchema.index({
+    id: 1,
+    title: 1,
+    moduleID: 1
+}, {
+    unique: true
+});
+
+const MarkSheet = mongoose.model('MarkSheet', MarkSheetSchema);
+module.exports = MarkSheet;
