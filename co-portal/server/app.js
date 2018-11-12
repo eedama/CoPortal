780767116901d2cbabd9f9674de059409412917f
Lavhe <jsirwali@retrorabbit.co.za>
2018-11-12 10:23:27 +0200

    Going live

--- co-portal/server/app.js
+++ co-portal/server/app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
@@ -42,7 +42,7 @@ app.use(function (req, res, next) {
 });
 
 mongoose
-    .connect("mongodb://localhost:27017/CoportalGPDevDB?authSource=admin", {
+    .connect("mongodb://localhost:27017/CoportalGPLiveDB?authSource=admin", {
         auth: {
             user: "admin",
             password: "Mulavhelesi@1"
@@ -50,7 +50,7 @@ mongoose
         useNewUrlParser: true,
         reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
         reconnectInterval: 500, // Reconnect every 500ms
-        dbName: "CoportalGPDevDB"
+        dbName: "CoportalGPLiveDB"
     })
     .then(answer => {
         console.log("Successfully connected to MONGO!");
@@ -65,7 +65,7 @@ app.use("/n", notificationController);
 app.use("/survey", surveyController);
 
 /// catch 404 and forwarding to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     var err = new Error("Not Found");
     err.status = 404;
     next(err);
@@ -76,7 +76,7 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get("env") === "development") {
-    app.use(function (err, req, res, next) {
+    app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render("error", {
             message: err.message,
@@ -87,7 +87,7 @@ if (app.get("env") === "development") {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render("error", {
         message: err.message,
