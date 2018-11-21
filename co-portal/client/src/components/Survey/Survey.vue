174d5e1e92e600e26f637cece80c9111bad1ff43
Sirwali Joseph <mulavhe@gmail.com>
2018-11-21 20:08:34 +0200

    Added the brief intro on survey

--- co-portal/client/src/components/Survey/Survey.vue
+++ co-portal/client/src/components/Survey/Survey.vue
@@ -1,12 +1,46 @@
 <template>
   <div>
-    <div class="row" v-if="currentPage == 0">
+     <div class="row" v-if="currentPage == -1">
       <div class="col s10 offset-s1 m8 offset-m2 row card-panel">
         <div class="col s12">
-          <h4>Co-Portal Feedback Survey</h4>
           <div class="card-image col l8 offset-l2 m6 offset-m3 s12">
             <img class="img-responsive" src="static/img/coPortalLogo.jpg">
           </div>
+        </div>
+        <div class="col s12">
+          <div class="row">
+            <div class="col s8 offset-s2 m6 offset-m3 text-center">
+              <span class="text-center">Co-portal is a school management system that 63Solutions is currently introducing to schools, the system is currently operating in one school in Rustenburg and the following are some of the key functions that the system does:</span>
+            </div>
+          </div>
+          <div class="row">
+          <div v-for="(feature,i) in features" :key="i" class="col s8 offset-s2 m6 offset-m3 text-center card-panel hoverable pointer waves-effect">
+            <p>{{ i + 1 }}. {{ feature }}</p>
+          </div>
+         </div>
+        </div>
+        <div class="row">
+          <div class="col s12 center-align">
+            <p>We would really appreciate it if you take part in the survey below.</p>
+          </div>
+          <div v-on:click="GoToNextPage(false)" class="col m4 offset-m4 s12 pointer center-align waves-effect">
+            <div class="card-panel hoverable">
+              <h5 class="text-xs-center">Take the survey <i class="material-icons right">exit_to_app</i> </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row" v-if="currentPage == 0">
+      <div class="col s10 offset-s1 m8 offset-m2 row card-panel">
+        <div class="col s12">
+          <h5><strong>Feedback Survey</strong></h5>
+          <div class="card-image col l8 offset-l2 m6 offset-m3 s12">
+            <img class="img-responsive" src="static/img/coPortalLogo.jpg">
+          </div>
+            <md-button v-on:click="currentPage = -1" class="md-fab">
+              <md-icon>close</md-icon>
+            </md-button>
         </div>
         <div class="col s12">
           <div class="row" v-for="(survey,i) in Survey" :key="i">
@@ -37,7 +71,7 @@
     <div class="row" v-if="currentPage == 1">
       <div class="col s10 offset-s1 m8 offset-m2 row card-panel">
         <div class="col s12">
-          <h4>Co-Portal Feedback Survey</h4>
+          <h5><strong>Feedback Survey</strong></h5>
           <div class="card-image col l8 offset-l2 m6 offset-m3 s12">
             <img class="img-responsive" src="static/img/coPortalLogo.jpg">
           </div>
@@ -103,6 +137,13 @@ export default {
   },
   data() {
     return {
+      features:[
+        "Serves as a communication tool between a teacher, parent and a learner",
+        "Allows a student to write an online assessment and the system marks the assessment and gives results to both students and parents",
+        "Allows a teacher to send out previous questions papers or any study material to a student",
+        "Allows a teacher to post test results of each subject that a learner is doing and parents will monitor how their children are performing at school",
+        "Provides career guidance as we have noticed that a lot of learners do not know which career path to follow after matric"
+      ],
       txtError: "",
       Questionare: [],
       surveyAnswers: [
@@ -130,7 +171,7 @@ export default {
           Answer: null
         }
       ],
-      currentPage: 0,
+      currentPage: -1,
       provinces: [
         {
           name: "Eastern Cape",
@@ -198,7 +239,9 @@ export default {
   methods: {
     GoToNextPage(final) {
       this.txtError = "";
-      if (!final) {
+      if(this.currentPage == -1){
+        this.currentPage = 0;
+      }else if (!final) {
         var arr = this.Survey.reverse();
         arr.forEach(survey => {
           if (
