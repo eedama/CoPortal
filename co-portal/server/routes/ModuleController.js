77fef1ea34d72146fcbb106f6c2ae1e76f712253
Sirwali Joseph <mulavhe@gmail.com>
2018-12-09 21:40:56 +0200

    ....

--- co-portal/server/routes/ModuleController.js
+++ co-portal/server/routes/ModuleController.js
@@ -93,7 +93,7 @@ router.get("/marksheet/for/:userID/moduleID/:moduleID", function (req, res) {
     var studentMarks = [];
     markSheet.forEach(m => {
       let obj = m;
-      obj.mark = m.studentMarks.filter(sm => sm.studentID == userID)[0].mark;
+      obj.mark = m.studentMarks.filter(sm => sm.studentID == userID)[0];
       obj.studentMarks = undefined;
       studentMarks.push(obj);
     });
