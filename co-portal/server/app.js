88f0ffca900b74f9cebeb99632166ad89ff265c9
Lavhe <jsirwali@retrorabbit.co.za>
2019-01-23 10:39:48 +0200

    Build for co-portal gpark

--- co-portal/server/app.js
+++ co-portal/server/app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
@@ -43,7 +43,7 @@ app.use(function (req, res, next) {
 });
 
 mongoose
-    .connect("mongodb://localhost:27017/CoportalGPDevDB?authSource=admin", {
+    .connect("mongodb://localhost:27017/CoportalGParkDB?authSource=admin", {
         auth: {
             user: "admin",
             password: "Mulavhelesi@1"
@@ -66,7 +66,7 @@ app.use("/n", notificationController);
 app.use("/survey", surveyController);
 
 /// catch 404 and forwarding to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     var err = new Error("Not Found");
     err.status = 404;
     next(err);
@@ -77,7 +77,7 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get("env") === "development") {
-    app.use(function (err, req, res, next) {
+    app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render("error", {
             message: err.message,
@@ -88,7 +88,7 @@ if (app.get("env") === "development") {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render("error", {
         message: err.message,
