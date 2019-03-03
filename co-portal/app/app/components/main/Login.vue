c4618c5b5002f155d43e2e7b30d995926821ac49
Sirwali Joseph <jsirwali@retrorabbit.co.za>
2019-03-03 05:45:44 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- co-portal/app/app/components/main/Login.vue
+++ co-portal/app/app/components/main/Login.vue
@@ -2,108 +2,32 @@
   <page class="bg-white" actionBarHidden="true">
     <GridLayout rows="*">
       <StackLayout verticalAlignment="center" row="0" class="m-y-10">
-        <Image
-          src="~/assets/images/coPortalLogo.png"
-          stretch="aspectFit"
-          verticalAlignment="bottom"
-          textAlignment="center"
-          class="m-10 bottomLogo"
-        ></Image>
+        <Image src="~/assets/images/coPortalLogo.png" stretch="aspectFit" verticalAlignment="bottom" textAlignment="center" class="m-10 bottomLogo"></Image>
         <ScrollView width="100%">
-          <CardView
-            verticalAlignment="center"
-            padding="10"
-            margin="25"
-            elevation="10"
-            shadowOffsetHeight="10"
-            shadowOpacity="0.2"
-            shadowRadius="50"
-          >
+          <CardView verticalAlignment="center" padding="10" margin="25" elevation="10" shadowOffsetHeight="10" shadowOpacity="0.2" shadowRadius="50">
             <GridLayout width="100%">
-              <FlexboxLayout
-                class="m-10"
-                justifyContent="space-between"
-                width="100%"
-                alignSelf="center"
-                height="100%"
-                flexDirection="column"
-              >
+              <FlexboxLayout class="m-10" justifyContent="space-between" width="100%" alignSelf="center" height="100%" flexDirection="column">
                 <GridLayout class="m-10 text-dark-black" rows="auto,auto" columns="auto,*">
-                  <label
-                    row="0"
-                    rowSpan="2"
-                    col="0"
-                    verticalAlignment="center"
-                    textAlignment="center"
-                    class="mdi m-10"
-                    fontSize="25%"
-                    :text="'mdi-email' | fonticon"
-                  ></label>
-                  <label
-                    row="0"
-                    col="1"
-                    class="h3 font-weight-bold text-mute text-dark-black"
-                    text="Email"
-                  ></label>
-                  <TextField
-                    row="1"
-                    col="1"
-                    keyboardType="email"
-                    returnKeyType="next"
-                    v-model="username"
-                    autocorrect="true"
-                    autocapitalizationType="none"
-                  ></TextField>
+                  <label row="0" rowSpan="2" col="0" verticalAlignment="center" textAlignment="center" class="mdi m-10" fontSize="25%" :text="'mdi-email' | fonticon"></label>
+                  <label row="0" col="1" class="h3 font-weight-bold text-mute text-dark-black" text="Email"></label>
+                  <TextField row="1" col="1" keyboardType="email" returnKeyType="next" v-model="username" autocorrect="true" autocapitalizationType="none"></TextField>
                 </GridLayout>
-
+  
                 <GridLayout class="m-10 text-dark-black" rows="auto,auto" columns="auto,*">
-                  <label
-                    row="0"
-                    rowSpan="2"
-                    col="0"
-                    verticalAlignment="center"
-                    textAlignment="center"
-                    class="mdi m-10"
-                    fontSize="25%"
-                    :text="'mdi-lock' | fonticon"
-                  ></label>
-                  <label
-                    row="0"
-                    col="1"
-                    class="h3 font-weight-bold text-mute text-dark-black"
-                    text="Password"
-                  ></label>
-                  <TextField
-                    row="1"
-                    col="1"
-                    ref="password"
-                    secure="true"
-                    returnKeyType="done"
-                    v-model="password"
-                    @returnPress="submit()"
-                    :class="{ light: !isLoading }"
-                  ></TextField>
+                  <label row="0" rowSpan="2" col="0" verticalAlignment="center" textAlignment="center" class="mdi m-10" fontSize="25%" :text="'mdi-lock' | fonticon"></label>
+                  <label row="0" col="1" class="h3 font-weight-bold text-mute text-dark-black" text="Password"></label>
+                  <TextField row="1" col="1" ref="password" secure="true" returnKeyType="done" v-model="password" @returnPress="submit()" :class="{ light: !isLoading }"></TextField>
                 </GridLayout>
-
+  
                 <ActivityIndicator v-show="isLoading" :busy="isLoading"></ActivityIndicator>
-
+  
                 <StackLayout v-show="!isLoading">
-                  <Button
-                    text="Login"
-                    :isEnabled="!isLoading"
-                    class="submit-button bg-dark-black text-white"
-                    @tap="submit()"
-                  ></Button>
+                  <Button text="Login" :isEnabled="!isLoading" class="submit-button bg-dark-black text-white" @tap="submit()"></Button>
                 </StackLayout>
-
+  
                 <GridLayout class="m-10">
                   <Ripple @tap="GoToResetPassword()">
-                    <label
-                      textAlignment="center"
-                      class="text-mute text-light-black p-15"
-                      fontSize="14%"
-                      text="Forgot Password?"
-                    ></label>
+                    <label textAlignment="center" class="text-mute text-light-black p-15" fontSize="14%" text="Forgot Password?"></label>
                   </Ripple>
                 </GridLayout>
               </FlexboxLayout>
