0ccc709c17f72db4c6c5491d2f50e11013f08879
--save <terrenceilidio@gmail.com>
2019-04-02 18:27:27 +0200

    changed StudentList ui

--- co-portal/app/app/components/pages/components/Module.vue
+++ co-portal/app/app/components/pages/components/Module.vue
@@ -142,28 +142,35 @@
               <WrapLayout>
                 <ScrollView orientation="vertical" v-for="(_student,i) in studentList" :key="i">
                   <StackLayout v-for="(_module, index) in studentList[i].modules" :key="index">
-                    <cardView height="230" elevation="5" margin="10" v-if="module.code == _module.code">
-                      <Ripple >
+                    <cardView
+                      height="280"
+                      elevation="5"
+                      margin="10"
+                      v-if="module.code == _module.code"
+                    >
+                      <Ripple>
                         <GridLayout
-                          rows="auto,auto,auto,auto,auto,auto,auto,auto,"
+                          rows="auto,auto,auto,auto,auto,auto,auto,auto,auto"
                           columns="auto,*"
                           class="p-x-20 p-y-15"
                         >
-                          <StackLayout verticalAlignment="center" row="0">
-                            <label
-                              row="0"
-                              textAlignment="center"
-                              class="labelTitle m-t-2"
-                              :text="_student.firstname + ' ' + _student.lastname"
-                            ></label>
-                          </StackLayout>
+                          <label
+                            row="0"
+                            verticalAlignment="center"
+                            textAlignment="center"
+                            fontSize="20"
+                            colspan="2"
+                            class="labelTitle text-dark-black"
+                            :text="_student.firstname + ' ' + _student.lastname"
+                          ></label>
+
                           <label
                             row="1"
                             col="0"
                             rowspan="2"
-                            class="text-dark-black text-dark-black mdi"
+                            class="text-dark-black mdi"
                             textAlignment="left"
-                            fontSize="20%"
+                            fontSize="35"
                             verticalAlignment="center"
                             :text="'mdi-account' | fonticon "
                           ></label>
@@ -179,7 +186,7 @@
                             row="2"
                             col="2"
                             fontSize="15"
-                            class="font-weight-bold"
+                            class="text-dark-black"
                             textAlignment="left"
                             :text="_student.idNumber"
                           ></label>
@@ -187,7 +194,7 @@
                             row="3"
                             col="0"
                             rowspan="2"
-                            fontSize="20%"
+                            fontSize="35"
                             class="font-weight-bold m-r-20 p-t-5 text-dark-black mdi"
                             :text="'mdi-human-male-female' | fonticon"
                           ></label>
@@ -195,7 +202,7 @@
                             row="3"
                             col="1"
                             textAlignment="left"
-                            class="font-weight-bold p-t-5 text-dark-black"
+                            class="font-weight-bold text-dark-black"
                             verticalAlignment="center"
                             fontSize="15"
                             text="Gender"
@@ -205,13 +212,14 @@
                             col="1"
                             fontSize="15"
                             textAlignment="left"
+                            class="text-dark-black"
                             :text="_student.gender"
                           ></label>
                           <label
                             row="5"
                             col="0"
                             rowspan="2"
-                            fontSize="20%"
+                            fontSize="35"
                             class="font-weight-bold m-r-20 p-t-5 mdi text-dark-black"
                             :text="'mdi-flag' | fonticon"
                           ></label>
@@ -227,14 +235,14 @@
                           <label
                             row="6"
                             col="1"
-                            class="font-weight-bold"
+                            class="text-dark-black"
                             v-if="_student.isSouthAfrican == true"
                             text="South African"
                           ></label>
                           <label
                             row="6"
                             col="1"
-                            class="font-weight-bold"
+                            class="text-dark-black"
                             v-if="_student.isSouthAfrican == false"
                             text="International Student"
                           ></label>
@@ -243,7 +251,7 @@
                             col="0"
                             rowspan="2"
                             class="font-weight-bold m-r-20 p-t-10 mdi text-dark-black"
-                            fontSize="20%"
+                            fontSize="30"
                             :text="'mdi-book-open-variant' | fonticon"
                           ></label>
                           <label
--- co-portal/app/app/components/pages/components/StudentList.vue
+++ co-portal/app/app/components/pages/components/StudentList.vue
@@ -1,127 +1,140 @@
 <template>
   <page actionBarHidden="true">
     <GridLayout rows="auto,*" columns="*">
-      <ScrollView orientation="vertical">
-        <StackLayout>
-          <cardView
-            v-for="(_student,i) in studentList"
-            :key="i"
-            height="240"
-            elevation="5"
-            margin="10"
-            textAlignment="center"
-          >
-            <ScrollView width="100%">
-              <GridLayout
-                rows="auto,auto,auto,auto,auto,auto,auto,auto,auto"
-                columns="auto,*"
-                class="p-x-20 p-y-15"
-              >
-                <StackLayout verticalAlignment="center" row="0">
-                  <label
-                    row="0"
-                    textAlignment="center"
-                    class="labelTitle m-t-2"
-                    :text="_student.firstname + ' ' + _student.lastname"
-                  ></label>
-                </StackLayout>
-                <label
-                  row="1"
-                  col="0"
-                  rowspan="2"
-                  class="text-dark-black text-dark-black mdi"
-                  textAlignment="left"
-                  fontSize="20%"
-                  verticalAlignment="center"
-                  :text="'mdi-account' | fonticon "
-                ></label>
-                <label
-                  row="1"
-                  col="1"
-                  verticalAlignment="center"
-                  textAlignment="left"
-                  class="font-weight-bold m-t-0 text-dark-black"
-                  text="IDNumber"
-                ></label>
-                <label
-                  row="2"
-                  col="2"
-                  fontSize="15"
-                  class="font-weight-bold"
-                  textAlignment="left"
-                  :text="_student.idNumber"
-                ></label>
-                <label
-                  row="3"
-                  col="0"
-                  rowspan="2"
-                  fontSize="20%"
-                  class="font-weight-bold m-r-20 p-t-5 text-dark-black mdi"
-                  :text="'mdi-human-male-female' | fonticon"
-                ></label>
-                <label
-                  row="3"
-                  col="1"
-                  textAlignment="left"
-                  class="font-weight-bold p-t-5 text-dark-black"
-                  verticalAlignment="center"
-                  fontSize="15"
-                  text="Gender"
-                ></label>
-                <label row="4" col="1" fontSize="15" textAlignment="left" :text="_student.gender"></label>
-                <label
-                  row="5"
-                  col="0"
-                  rowspan="2"
-                  fontSize="20%"
-                  class="font-weight-bold m-r-20 p-t-5 mdi text-dark-black"
-                  :text="'mdi-flag' | fonticon"
-                ></label>
-                <label
-                  row="5"
-                  col="1"
-                  textAlignment="left"
-                  class="font-weight-bold p-t-5 text-dark-black"
-                  verticalAlignment="center"
-                  fontSize="15"
-                  text="Nationality"
-                ></label>
-                <label
-                  row="6"
-                  col="1"
-                  class="font-weight-bold"
-                  v-if="_student.isSouthAfrican == true"
-                  text="South African"
-                ></label>
-                <label
-                  row="7"
-                  col="0"
-                  rowspan="2"
-                  class="font-weight-bold m-r-20 p-t-10 mdi text-dark-black"
-                  fontSize="20%"
-                  :text="'mdi-book-open-variant' | fonticon"
-                ></label>
-                <label
-                  row="7"
-                  col="1"
-                  verticalAlignment="center"
-                  textAlignment="left"
-                  class="font-weight-bold p-t-5 text-dark-black"
-                  text="Modules"
-                ></label>
-                <label
-                  row="8"
-                  col="1"
-                  class="font-weight-bold mdi"
-                  v-for="(_module,i) in studentList.modules"
-                  :key="i"
-                  :text="_module.code[i] + ',' "
-                ></label>
-              </GridLayout>
-            </ScrollView>
-          </cardView>
-        </StackLayout>
-      </ScrollView>
+      <StackLayout row="0">
+        <WrapLayout>
+          <ScrollView orientation="vertical" v-for="(_student,i) in studentList" :key="i">
+            <StackLayout v-for="(_module, index) in studentList[i].modules" :key="index">
+              <cardView height="280" elevation="5" margin="10">
+                <Ripple>
+                  <GridLayout
+                    rows="auto,auto,auto,auto,auto,auto,auto,auto,auto"
+                    columns="auto,*"
+                    class="p-x-20 p-y-15"
+                  >
+                    <label
+                      row="0"
+                      verticalAlignment="center"
+                      textAlignment="center"
+                      fontSize="20"
+                      colspan="2"
+                      class="labelTitle text-dark-black"
+                      :text="_student.firstname + ' ' + _student.lastname"
+                    ></label>
+
+                    <label
+                      row="1"
+                      col="0"
+                      rowspan="2"
+                      class="text-dark-black mdi"
+                      textAlignment="left"
+                      fontSize="35"
+                      verticalAlignment="center"
+                      :text="'mdi-account' | fonticon "
+                    ></label>
+                    <label
+                      row="1"
+                      col="1"
+                      verticalAlignment="center"
+                      textAlignment="left"
+                      class="font-weight-bold m-t-0 text-dark-black"
+                      text="IDNumber"
+                    ></label>
+                    <label
+                      row="2"
+                      col="2"
+                      fontSize="15"
+                      class="text-dark-black"
+                      textAlignment="left"
+                      :text="_student.idNumber"
+                    ></label>
+                    <label
+                      row="3"
+                      col="0"
+                      rowspan="2"
+                      fontSize="35"
+                      class="font-weight-bold m-r-20 p-t-5 text-dark-black mdi"
+                      :text="'mdi-human-male-female' | fonticon"
+                    ></label>
+                    <label
+                      row="3"
+                      col="1"
+                      textAlignment="left"
+                      class="font-weight-bold p-t-5 text-dark-black"
+                      verticalAlignment="center"
+                      fontSize="15"
+                      text="Gender"
+                    ></label>
+                    <label
+                      row="4"
+                      col="1"
+                      fontSize="15"
+                      textAlignment="left"
+                      class="text-dark-black"
+                      :text="_student.gender"
+                    ></label>
+                    <label
+                      row="5"
+                      col="0"
+                      rowspan="2"
+                      fontSize="35"
+                      class="font-weight-bold m-r-20 p-t-5 mdi text-dark-black"
+                      :text="'mdi-flag' | fonticon"
+                    ></label>
+                    <label
+                      row="5"
+                      col="1"
+                      textAlignment="left"
+                      class="font-weight-bold p-t-5 text-dark-black"
+                      verticalAlignment="center"
+                      fontSize="15"
+                      text="Nationality"
+                    ></label>
+                    <label
+                      row="6"
+                      col="1"
+                      class="text-dark-black"
+                      v-if="_student.isSouthAfrican == true"
+                      text="South African"
+                    ></label>
+                    <label
+                      row="6"
+                      col="1"
+                      class="text-dark-black"
+                      v-if="_student.isSouthAfrican == false"
+                      text="International Student"
+                    ></label>
+                    <label
+                      row="7"
+                      col="0"
+                      rowspan="2"
+                      class="font-weight-bold m-r-20 p-t-10 mdi text-dark-black"
+                      fontSize="30"
+                      :text="'mdi-book-open-variant' | fonticon"
+                    ></label>
+                    <label
+                      row="7"
+                      col="1"
+                      verticalAlignment="center"
+                      textAlignment="left"
+                      class="font-weight-bold p-t-5 text-dark-black"
+                      text="Modules"
+                    ></label>
+                    <label
+                      row="8"
+                      col="1"
+                      class="font-weight-bold mdi"
+                      v-for="(_module,i) in studentList.modules"
+                      :key="i"
+                      :text="_module.code[i] + ',' "
+                    ></label>
+                  </GridLayout>
+                </Ripple>
+              </cardView>
+            </StackLayout>
+          </ScrollView>
+        </WrapLayout>
+      </StackLayout>
     </GridLayout>
   </page>
 </template>
@@ -182,9 +195,4 @@ export default {
 </script>
 
 <style lang="scss"scoped>
-.labelTitle {
-  color: black;
-  font-size: 20px;
-  font-weight: bold;
-}
 </style>
