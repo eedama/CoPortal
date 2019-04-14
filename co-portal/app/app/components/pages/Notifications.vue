6ab27103f2f32783bfc9b5e959f56eb0c4d3319a
--save <terrenceilidio@gmail.com>
2019-04-14 12:57:33 +0200

    .....

--- co-portal/app/app/components/pages/Notifications.vue
+++ co-portal/app/app/components/pages/Notifications.vue
@@ -2,20 +2,99 @@
   <page actionBarHidden="true">
     <ScrollView>
       <GridLayout rows="*,auto,auto,auto,auto" columns="*">
+        <StackLayout row="0">
+          <GridLayout rows="auto,auto" columns="*">
+            <label
+              row="0"
+              verticalAlignment="center"
+              textAlignment="center"
+              class="mdi p-15 text-dark-black"
+              fontSize="50%"
+              :text="'mdi-bell' | fonticon"
+            ></label>
+          </GridLayout>
+        </StackLayout>
 
-      <StackLayout row="0">
-        <GridLayout rows="auto,auto" columns="*">
-          <label row="0" verticalAlignment="center" textAlignment="center" class="mdi p-15 text-dark-black" fontSize="50%" :text="'mdi-bell' | fonticon"></label>
-        </GridLayout>
-      </StackLayout>
+        <StackLayout
+          row="0"
+          rowspan="2"
+          verticalAlignment="center"
+          textAlignment="center"
+          v-if="!isLoading && (!notification || notification.length == 0)"
+        >
+          <label
+            verticalAlignment="center"
+            textAlignment="center"
+            class="mdi m-x-10"
+            fontSize="50%"
+            :text="'mdi-alert' | fonticon"
+          ></label>
+          <label
+            verticalAlignment="center"
+            textAlignment="center"
+            class="m-10 font-weight-bold"
+            fontSize="20%"
+            text="No Notifications"
+          ></label>
+          <label
+            verticalAlignment="center"
+            textAlignment="center"
+            class="m-x-10"
+            fontSize="20%"
+            :textWrap="true"
+            text="You are up-to-date with all the announcements"
+          ></label>
+        </StackLayout>
 
-      <StackLayout row="0" rowSpan="2" verticalAlignment="center" textAlignment="center" v-if="!isLoading && (!notification || notification.length == 0)">
-        <label verticalAlignment="center" textAlignment="center" class="mdi m-x-10" fontSize="50%" :text="'mdi-alert' | fonticon"></label>
-        <label verticalAlignment="center" textAlignment="center" class="m-10 font-weight-bold" fontSize="30%" text="No Notifications"></label>
-        <label verticalAlignment="center" textAlignment="center" class="m-x-10" fontSize="20%" :textWrap="true" text="You are up-to-date with all the announcements"></label>
-      </StackLayout>;
-    
         <StackLayout row="1">
+        <label row="1" verticalAlignment="center" textAlignment="center" class="p-15 text-dark-black" fontSize="30%" text="General Notifications"></label>
+      
+        <StackLayout row="2">
+          <ActivityIndicator verticalAlignment="center"  textAlignment="center"  row="1"  v-show="isLoading"  :busy="isLoading" ></ActivityIndicator>
+          <CardView  v-for="notify in generalNotification"  :key="notify._id"  elevation="15"  margin="5">
+            <Ripple @tap="readMessage(notify.title,notify.message)">
+              <GridLayout class="p-15" rows="auto,auto" columns="auto,*,auto">
+                <Image
+                  row="0"
+                  col="0"
+                  rowspan="2"
+                  verticalAlignment="center"
+                  src="res://ic_logo"
+                  width="60"
+                  height="60"
+                  borderRadius="50%"
+                ></Image>
+                <label
+                  row="0"
+                  col="1"
+                  class="font-weight-bold p-x-5"
+                  fontSize="16%"
+                  :text="notify.title"
+                ></label>
+                <label
+                  row="0"
+                  col="2"
+                  class="h4 text-dark-black"
+                  :text="getMoment(notify.date).fromNow()"
+                ></label>
+                <label
+                  row="1"
+                  col="2"
+                  class="h4 text-dark-black"
+                  v-if="notify.moduleId"
+                  :text="notify.moduleId.name"
+                ></label>
+                <label row="1" col="1" class="text-dark-black p-x-5" :text="notify.message"></label>
+                <label
+                  row="0"
+                  class="font-weight-bold p-x-5"
+                  fontSize="20%"
+                  v-if="notify.moduleId != null"
+                  text="No Notifications"
+                ></label>
+              </GridLayout>
+            </Ripple>
+          </CardView>
         <label row="1" verticalAlignment="center" textAlignment="center" class="p-15 text-dark-black" fontSize="30%" text="General Notifications"></label>
       
         <StackLayout row="2">
@@ -64,9 +143,95 @@
               </GridLayout>
             </Ripple>
           </CardView>
-        </StackLayout>
+          <label
+            row="1"
+            verticalAlignment="center"
+            textAlignment="center"
+            class="p-15 text-dark-black"
+            fontSize="30%"
+            text="General Notifications"
+          ></label>
 
-         </StackLayout>
+          <StackLayout row="2">
+            <ActivityIndicator
+              verticalAlignment="center"
+              textAlignment="center"
+              row="1"
+              v-show="isLoading"
+              :busy="isLoading"
+            ></ActivityIndicator>
+            <CardView
+              v-for="notify in generalNotification"
+              :key="notify._id"
+              elevation="15"
+              margin="5"
+            >
+              <Ripple @tap="readMessage(notify.title,notify.message)">
+                <GridLayout class="p-15" rows="auto,auto" columns="auto,*,auto">
+                  <Image
+                    row="0"
+                    col="0"
+                    rowspan="2"
+                    verticalAlignment="center"
+                    src="res://ic_logo"
+                    width="60"
+                    height="60"
+                    borderRadius="50%"
+                  ></Image>
+                  <label
+                    row="0"
+                    col="1"
+                    class="font-weight-bold p-x-5"
+                    fontSize="16%"
+                    :text="notify.title"
+                  ></label>
+                  <label
+                    row="0"
+                    col="2"
+                    class="h4 text-dark-black"
+                    :text="getMoment(notify.date).fromNow()"
+                  ></label>
+                  <label
+                    row="1"
+                    col="2"
+                    class="h4 text-dark-black"
+                    v-if="notify.moduleId"
+                    :text="notify.moduleId.name"
+                  ></label>
+                  <label row="1" col="1" class="text-dark-black p-x-5" :text="notify.message"></label>
+                  <label
+                    row="0"
+                    class="font-weight-bold p-x-5"
+                    fontSize="20%"
+                    v-if="notify.moduleId != null"
+                    text="No Notifications"
+                  ></label>
+                </GridLayout>
+              </Ripple>
+            </CardView>
+            <StackLayout
+              row="1"
+              verticalAlignment="center"
+              textAlignment="center"
+              v-if="!isLoading && generalNotification.length == 0"
+            >
+              <label
+                verticalAlignment="center"
+                textAlignment="center"
+                class="mdi m-x-10"
+                fontSize="50%"
+                :text="'mdi-alert' | fonticon"
+              ></label>
+              <label
+                verticalAlignment="center"
+                textAlignment="center"
+                class="m-x-10 font-weight-bold"
+                fontSize="20%"
+                text="No Notifications"
+              ></label>
+            </StackLayout>
+          </StackLayout>
+        </StackLayout>
 
         <StackLayout row="3">
           <GridLayout rows="auto" columns="*">
@@ -82,8 +247,19 @@
         </StackLayout>
 
         <StackLayout row="4">
-          <ActivityIndicator  verticalAlignment="center"  textAlignment="center"  row="1"  v-show="isLoading"  :busy="isLoading"  ></ActivityIndicator>
-          <CardView  v-for="notify in moduleNotification"  :key="notify._id"  elevation="15"  margin="5"  >
+          <ActivityIndicator
+            verticalAlignment="center"
+            textAlignment="center"
+            row="1"
+            v-show="isLoading"
+            :busy="isLoading"
+          ></ActivityIndicator>
+          <CardView
+            v-for="notify in moduleNotification"
+            :key="notify._id"
+            elevation="15"
+            margin="5"
+          >
             <Ripple @tap="readMessage(notify.title,notify.message)">
               <GridLayout class="p-15" rows="auto,auto,auto" columns="auto,*,auto">
                 <Image
@@ -127,10 +303,29 @@
               </GridLayout>
             </Ripple>
           </CardView>
-
+          <StackLayout
+            row="1"
+            verticalAlignment="center"
+            textAlignment="center"
+            v-if="!isLoading && moduleNotification.length == 0"
+          >
+            <label
+              verticalAlignment="center"
+              textAlignment="center"
+              class="mdi m-x-10"
+              fontSize="50%"
+              :text="'mdi-alert' | fonticon"
+            ></label>
+            <label
+              verticalAlignment="center"
+              textAlignment="center"
+              class="m-x-10 font-weight-bold"
+              fontSize="20%"
+              text="No Notifications"
+            ></label>
+          </StackLayout>
         </StackLayout>
       </GridLayout>
-
     </ScrollView>
   </page>
 </template>
@@ -157,13 +352,13 @@ export default {
       .then(notifications => {
         this.notification = JSON.parse(JSON.stringify(notifications));
         this.isLoading = false;
-          this.notification.forEach(notificationType => {
-            if (notificationType.moduleId == null) {
-          this.generalNotification.push(notificationType);
-        } else {
-          this.moduleNotification.push(notificationType);
-        }
-      });
+        this.notification.forEach(notificationType => {
+          if (notificationType.moduleId == null) {
+            this.generalNotification.push(notificationType);
+          } else {
+            this.moduleNotification.push(notificationType);
+          }
+        });
       })
       .catch(err => {
         this.isLoading = false;
@@ -173,7 +368,6 @@ export default {
           duration: 3000
         });
       });
-    
   },
   methods: {
     pageLoaded() {
