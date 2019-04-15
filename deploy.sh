05fcc2a6005e5124f5448f6a76befb77da93725d
Joe Sirwali <mulavhe@gmail.com>
2019-04-15 15:47:15 +0200

    Added travis build files

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PASSWORD=$1;
+PORT=$2;
+USERNAME=$3;
+ADDRESS=$4;
+PROJECT_FOLDER=$5;
+PROJECT_PORT=$6;
+PROJECT_NAME=$7;
+FILE_NAME="running-$6-$7-$(date +"%F___%T").txt";
+
+# Copy files to server
+echo "Firing a build and deploy to $PROJECT_FOLDER" >> "/tmp/$FILE_NAME";
+sshpass -p "$PASSWORD" rsync --progress -azvru --delete --update --exclude='.git/' --filter=":- .gitignore" --exclude='.travis.yml' -e "ssh -p $PORT -oStrictHostKeyChecking=no" "./co-portal/" "$USERNAME@$ADDRESS:$PROJECT_FOLDER/";
+
+# Run build on server
+sshpass -p "$PASSWORD" ssh -t -p "$PORT" -oStrictHostKeyChecking=no "$USERNAME@$ADDRESS" "bash -s" < ./build.sh "$PROJECT_FOLDER" "$PROJECT_NAME" "$FILE_NAME";
