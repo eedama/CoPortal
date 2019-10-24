1f206985b6503dfa439955a2a2e8d5fb7bafcbc5
netshedzo <mudau.n4@gmail.com>
2019-10-24 15:47:36 +0200

    added survey lecture side handling

--- app/app/components/pages/components/Survey.vue
+++ app/app/components/pages/components/Survey.vue
@@ -28,10 +28,114 @@
           androidSelectedTabHighlightColor="black"
         >
           <TabViewItem title="New">
-            <ScrollView height="300"> </ScrollView>
+            <ScrollView height="300">
+              <StackLayout
+                class="m-x-2"
+                :textWrap="true"
+                width="90%"
+                horizontalAlignment="center"
+              >
+                <ActivityIndicator
+                  v-show="loading"
+                  row="0"
+                  verticalAlignment="center"
+                  class="p-t-30 text-blue-black"
+                  textAlignment="center"
+                  :busy="loading"
+                ></ActivityIndicator>
+                <label
+                  row="0"
+                  class="p-y-20"
+                  v-if="!loading && surveyTemplates.length > 0"
+                  color="gray"
+                  textAlignment="center"
+                  fontWeight="bold"
+                  verticalAlignment="top"
+                  fontSize="13%"
+                  text="please choose a survey you want the students to take"
+                ></label>
+                <label
+                  row="0"
+                  class="p-y-20"
+                  v-if="!loading && surveyTemplates.length == 0"
+                  color="gray"
+                  textAlignment="center"
+                  fontWeight="bold"
+                  verticalAlignment="top"
+                  fontSize="15%"
+                  text="this module has no surveys to take"
+                ></label>
+                <CheckBox
+                  font-size="20"
+                  colSpan="2"
+                  class="checkbox"
+                  boxType="circle"
+                  fillColor="#006064"
+                  :checked="selectedTemplate == answer._id"
+                  :text="answer.title"
+                  v-for="(answer, j) in surveyTemplates"
+                  :key="j"
+                  @tap="selectedTemplate = answer._id"
+                ></CheckBox>
+                <Button
+                  horizontalAlignment="center"
+                  verticalAlignment="top"
+                  v-if="!loading && surveyTemplates.length > 0"
+                  colSpan="2"
+                  text="Create Survey"
+                  class="text-white submit-button bg-blue-black m-t-5"
+                  @tap="createSurvey()"
+                ></Button>
+              </StackLayout>
+            </ScrollView>
           </TabViewItem>
           <TabViewItem title="History">
-            <ScrollView height="300"> </ScrollView>
+            <ScrollView height="300">
+              <StackLayout>
+                <CardView
+                  elevation="5"
+                  margin="5"
+                  v-for="(survey, i) in history"
+                  :key="i"
+                >
+                  <Ripple>
+                    <GridLayout
+                      class="text-dark-black p-15"
+                      rows="auto,auto"
+                      columns="auto,*,auto"
+                    >
+                      <label
+                        row="0"
+                        col="0"
+                        class="mdi text-dark-black m-r-20"
+                        rowSpan="2"
+                        verticalAlignment="center"
+                        textAlignment="left"
+                        fontSize="35"
+                        :text="'mdi-comment-question-outline' | fonticon"
+                      ></label>
+                      <label
+                        row="0"
+                        col="1"
+                        colSpan="2"
+                        class="font-weight-bold"
+                        fontSize="15"
+                        textAlignment="left"
+                        :textWrap="true"
+                        :text="survey.surveyTemplateId.title"
+                      ></label>
+                      <label
+                        row="1"
+                        col="1"
+                        fontSize="15"
+                        textAlignment="left"
+                        :text="getDate(survey.date)"
+                      ></label>
+                    </GridLayout>
+                  </Ripple>
+                </CardView>
+              </StackLayout>
+            </ScrollView>
           </TabViewItem>
         </TabView>
       </StackLayout>
@@ -46,25 +150,72 @@ import * as connectivity from "tns-core-modules/connectivity";
 import date from "date-and-time";
 export default {
   data() {
-    return {};
+    return {
+      selectedTemplate: "",
+      surveyTemplates: [],
+      loading: false,
+      history: [],
+      showStudents: false,
+      students: []
+    };
   },
   mounted() {
     this.lectureId = this.$store.state.cache.cachedUser.user._id;
+    this.getSurveyTemplates();
+    this.getHistory();
   },
   props: ["module"],
   methods: {
-    getHistory() {
+    getDate(dt) {
+      return date.format(new Date(dt), "DD MMMM YYYY");
+    },
+    createSurvey() {
+      if (this.selectedTemplate === "") {
+        this.$feedback.warning({
+          title: "Error",
+          message: "please choose a survey for students to perform"
+        });
+        return;
+      }
+      this.loading = true;
       this.$api
-        .getAttendanceHistory(this.module._id)
+        .submitSurvey(this.selectedTemplate)
+        .then(attend => {
+          this.loading = false;
+          this.$feedback.success({
+            title: attend.toString()
+          });
+        })
+        .catch(err => {
+          this.loading = false;
+          this.$feedback.error({
+            title: err.message
+          });
+        });
+    },
+    getSurveyTemplates() {
+      this.loading = true;
+      this.$api
+        .getSurveyTemplate(this.module._id)
         .then(attend => {
           const days = JSON.parse(JSON.stringify(attend));
-          this.history = days.map(i => {
-            return {
-              name: date.format(new Date(i.date), "DD MMMM YYYY"),
-              _id: i._id,
-              date: i.date
-            };
+          this.surveyTemplates = days;
+          this.loading = false;
+        })
+        .catch(err => {
+          this.$feedback.error({
+            title: err.message
           });
+          this.loading = false;
+        });
+    },
+    getHistory() {
+      this.$api
+        .getSurveyHistory(this.module._id)
+        .then(attend => {
+          const days = JSON.parse(JSON.stringify(attend));
+          this.history = days;
+          console.log("history", days);
         })
         .catch(err => {
           this.$feedback.error({
