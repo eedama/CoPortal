58ae71a409911d991f39f6308a6732d067c775bc
Sirwali Joe <mulavhe@gmail.com>
2019-11-26 11:06:26 +0200

    Added a build script

---
+++ server/tests/api.test.ts
@@ -0,0 +1,17 @@
+export { };
+import mocha from 'mocha';
+import smsProvider from "../services/SMSProvider"
+import emailProvider from "../services/EmailProvider"
+require('dotenv').config();
+
+describe('Test sending EMAIL', () => {
+    it('responds with json', async () => {
+        return emailProvider.sendEmail("mulavhe@gmail.com", `Coportal - ${process.env.COPORTAL_DEV}`, `Running a ${process.env.NODE_ENV} Travis test \n\n ${new Date()}`);
+    });
+})
+
+describe('Test sending SMS', () => {
+    it('responds with json', async () => {
+        return smsProvider.sendSMS('+27760487292', `Running a Coportal ${process.env.NODE_ENV} Travis test \n\n ${new Date()}`);
+    });
+})
